e2b1191f76187e930c3c4f7d0d87bb481df14a3a
walkinnight <liluhua233@gmail.com>
2020-09-15 11:53:48 +0000

    上传头像，显示头像，限制头像规格

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -27,8 +28,18 @@ class UsersController extends Controller
     }
 
     //更新用户信息
-    public function update(Request $request,User $user){
-        $user->update($request->all());
+    public function update(Request $request, ImageUploadHandler $uploader,User $user){
+
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
