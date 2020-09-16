e0d6edf1de787e101f9d31ae2fcef4f1eaee8540
walkinnight <liluhua233@gmail.com>
2020-09-16 11:44:44 +0000

    发表回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,18 +4,18 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    //统计回复数
+    public function created(Reply $reply)
     {
-        //
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
-    public function updating(Reply $reply)
+    public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -49,4 +49,6 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //上传话题图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//帖子回复
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
