c53498b91f23c9e8575cdedcb39af85c1bc843b2
walkinnight <liluhua233@gmail.com>
2020-09-16 07:41:44 +0000

    队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,8 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
+use App\Jobs\TranslateSlug;
 
 class TopicObserver
 {
@@ -25,12 +24,17 @@ class TopicObserver
         // XSS 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
 
-        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
-
         //创建摘要
         $topic->excerpt = make_excerpt($topic->body);
+    }
+
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
+        }
     }
 }
