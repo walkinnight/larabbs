cb9c3d29e0cddd6ee6a5ed7d42f814e512d27334
walkinnight <liluhua233@gmail.com>
2020-09-16 05:20:30 +0000

    删除帖子

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -66,7 +66,7 @@ class TopicsController extends Controller{
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
 	}
 
 	//删除话题
@@ -75,7 +75,7 @@ class TopicsController extends Controller{
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '成功1删除');
 	}
 
 	//编辑器上传图片
